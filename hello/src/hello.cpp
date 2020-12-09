26b2361c30175117980b4323d8fb80ff348fee3d
i14simizu <i14simizu@tokuyama.kosen-ac.jp>
2020-12-09 10:06:12 +0000

    helloの追加

---
+++ hello/src/hello.cpp
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <ros/ros.h>
+#include <ros/console.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "basic_logger");
+  ros::NodeHandle n;
+  ros::Rate loop_rate(1);
+  // if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug))
+  // {
+  //   ros::console::notifyLoggerLevelsChanged();
+  // }
+
+  int count = 0;
+  while (ros::ok())
+  {
+    ROS_DEBUG("log:%i", count);
+    ROS_INFO("log:%i", count);
+    ROS_WARN("log:%i", count);
+    ROS_ERROR("log:%i", count);
+    ROS_FATAL("log:%i", count);
+
+    count++;
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
