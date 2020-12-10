801b49963a7cf834158937b7b033995497e7acf8
i14simizu <i14simizu@tokuyama.kosen-ac.jp>
2020-12-10 15:52:36 +0000

    myteleopで文字列を読み取って前進後退

--- myteleop/src/test.cc
+++ myteleop/src/test.cc
@@ -1,14 +1,14 @@
 #include "ros/ros.h"
 
-#include <string>
-#include <sstream>
-#include <iostream>
-#include <geometry_msgs/Twist.h>
-#include <signal.h>
+#include <string.h> //strlen()を使用する
+#include <iostream> //入出力を使用する
+#include <geometry_msgs/Twist.h> //ROSのgeometry_msgs/Twist型を使用する
+#include <signal.h> //Ctrl-Cのシグナルを読み取って、ハンドラを起動するのに使用
+#include <cstring> //strncmp()を使用する
 
 #include "./test.hh"
 
-using namespace std;
+using namespace std; //おまじない
 
 void mysigintHandler(int sig){
     ROS_INFO("shutdown now.\n");
@@ -17,7 +17,7 @@ void mysigintHandler(int sig){
 }
 
 int main(int argc, char **argv){
-    ros::init(argc, argv, "myteleop", ros::init_options::NoSigintHandler); //ノードの名前を定義
+    ros::init(argc, argv, "myteleop", ros::init_options::NoSigintHandler); //ノードの名前を定義, SigintHandler()が自動的に呼ばれるのを阻止
     ros::NodeHandle n;
     ros::Publisher msg_pub = n.advertise<geometry_msgs::Twist>("/hexarobo_s/diff_drive_controller/cmd_vel", 1000); //cmd_velというトピックにgeometry_msgs::Twist型のメッセージを送る
     ros::Rate loop_rate(10);
@@ -25,16 +25,38 @@ int main(int argc, char **argv){
     signal(SIGINT, mysigintHandler);
 
     geometry_msgs::Twist twist;
-    bool flag = false;
-    char c;
+
+    //初期値の設定
+    twist.linear.x = 0.0;
+    twist.linear.y = 0.0;
+    twist.linear.z = 0.0;
+    twist.angular.x = 0.0;
+    twist.angular.y = 0.0;
+    twist.angular.z = 0.0;
+
+    double value = 0.0;
+    bool flag = false; //while()に1回目入る時の状態を記録
+    char str[50]; //入力された文字列を格納する配列
+    
+
     while(ros::ok()){
         if(!flag){
             ROS_INFO("start publish\n");
             flag = true;
         }
-        cin >> c; //キーの読み込み
-        cout << c << endl; //読み込んだキーを出力
+        
+        cin >> str;
+        cout << "you typewrite this : " << str << endl;
+        cout << "your type count : " << strlen(str) << endl;
 
+        if(strncmp(str, "rrr", 3)==0 && value<0.5){
+            value += 0.02;
+        }
+        else if(strncmp(str, "bbb", 3)==0 && value >= 0.0){
+            value -= 0.02;
+        }
+
+        twist.linear.x = value;
         msg_pub.publish(twist);
 
         ros::spinOnce();
