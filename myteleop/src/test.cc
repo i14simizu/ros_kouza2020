1913d4d0a992788985f87bcb06e9dc9702a54e4b
i14simizu <i14simizu@tokuyama.kosen-ac.jp>
2020-12-10 12:10:33 +0000

    シャットダウン処理の追記

--- myteleop/src/test.cc
+++ myteleop/src/test.cc
@@ -4,16 +4,24 @@
 #include <sstream>
 #include <iostream>
 #include <geometry_msgs/Twist.h>
+#include <signal.h>
 
 #include "./test.hh"
 
 using namespace std;
 
+void mysigintHandler(int sig){
+    ROS_INFO("shutdown now.\n");
+    ros::shutdown();
+}
+
 int main(int argc, char **argv){
-    ros::init(argc, argv, "myteleop"); //ノードの名前を定義
+    ros::init(argc, argv, "myteleop", ros::init_options::NoSigintHandler); //ノードの名前を定義
     ros::NodeHandle n;
     ros::Publisher msg_pub = n.advertise<geometry_msgs::Twist>("/hexarobo_s/diff_drive_controller/cmd_vel", 1000); //cmd_velというトピックにgeometry_msgs::Twist型のメッセージを送る
     ros::Rate loop_rate(10);
+
+    signal(SIGINT, mysigintHandler);
 
     geometry_msgs::Twist twist;
     bool flag = false;
