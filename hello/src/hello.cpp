8254b63e9eb3f6605da33766244b09fece835608
i14simizu <i14simizu@tokuyama.kosen-ac.jp>
2020-12-15 02:51:51 +0000

    helloの改定

--- hello/src/hello.cpp
+++ hello/src/hello.cpp
@@ -3,9 +3,9 @@
 #include <ros/console.h>
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "basic_logger");
+  ros::init(argc, argv, "hello_node");
   ros::NodeHandle n;
-  ros::Rate loop_rate(1);
+  ros::Rate loop_rate(5);
   // if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug))
   // {
   //   ros::console::notifyLoggerLevelsChanged();
@@ -14,13 +14,16 @@ int main(int argc, char** argv){
   int count = 0;
   while (ros::ok())
   {
-    ROS_DEBUG("log:%i", count);
-    ROS_INFO("log:%i", count);
-    ROS_WARN("log:%i", count);
-    ROS_ERROR("log:%i", count);
-    ROS_FATAL("log:%i", count);
+    ROS_DEBUG("log:%d", count);
+    ROS_INFO("log:%d", count);
+    ROS_WARN("log:%d", count);
+    ROS_ERROR("log:%d", count);
+    ROS_FATAL("log:%d", count);
 
     count++;
+    if(count>=10){
+      count=0;
+    }
 
     ros::spinOnce();
     loop_rate.sleep();
